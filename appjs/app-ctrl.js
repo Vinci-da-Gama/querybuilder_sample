5cd76c530e1a56fb78cbe85b1b81146334ef6168
Peter CHEN <yongdechen@gmail.com>
2015-10-28 13:51:58 +1100

    got to ng-include step...

--- appjs/app-ctrl.js
+++ appjs/app-ctrl.js
@@ -2,6 +2,7 @@
 	var ctrlM = angular.module('aqb.ctrl');
 
 	ctrlM.controller('QueryBuilderCtrl', ['$scope', function($scope){
+        var cs = $scope;
 		var tq = String.fromCharCode(9775)+" - ";
 
 		var data = '{"group": {"operator": "AND","rules": []}}';
@@ -10,10 +11,16 @@
             return String(str).replace(/</g, '&lt;').replace(/>/g, '&gt;');
         }
 
-        // This function would be called, Add Condition, Add Group and remove Group...
+        // This function would be called, Add Condition, Add Group and remove Group, Remove Rule...
         // The purpose is about build up the data object.
+        // why computed function only is triggered when Add Condition, Add Group, Remove Group and Remove Rule has been called?
+        // Because, at beginning, group is empty, thus, the inside if condition is triggered, then jump out this function.
         function computed(group) {
-
+            /*console.log('The process about how this computed function works...???');
+            console.log('why computed function only is triggered when Add Condition, Add Group, Remove Group and Remove Rule has been called? Because, at beginning, group is undefined, thus, the inside if condition is triggered, then jump over this function.');
+            console.log('when Add Condition or Add Group btns trigger their function. the group obj would be found.');
+            console.log('Then, it is not undefined group obj, it is valided group obj. the if condition inside this computed function would be ignored.');
+            console.log('Then, this computed function will generate and form the group data object.');*/
             if (!group) return "";
 
             for (var str = "(", i = 0; i < group.rules.length; i++) {
@@ -32,6 +39,7 @@
                 // console.log('group.rules[i].condition --> '+tq, group.rules[i].condition);
                 // 'group.rules[i].data --> the last field (input). It is one letter one time...
                 // console.log('group.rules[i].data --> '+tq, group.rules[i].data);
+                // console.log('typ of group.rules[i].data is --> '+typeof(group.rules[i].data));
                 // htmlEntities(group.rules[i].condition) only do one thing... if the condition has < or > , 
                 // those < or > would be replaced by &lt; &gt; -- then computer could understand.
                 // console.log('html htmlEntities -> '+tq+" : ", htmlEntities(group.rules[i].condition));
@@ -57,6 +65,13 @@
             /*console.log(tq+" $scope.json ", $scope.json);
             console.log(tq+" $scope.output ", $scope.output);*/
         }, true);
+
+        cs.myfields = [
+        { name: 'Firstname', url: './partials/template2.html' },
+        { name: 'Lastname', url: './partials/template2.html' },
+        { name: 'Birthdate', url: './partials/template2.html' },
+        { name: 'City', url: './partials/template1.html' },
+        { name: 'Country', url: './partials/template1.html' }];
 
 	}]);
 
--- appjs/query-builder.js
+++ appjs/query-builder.js
@@ -5,30 +5,45 @@
 		return {
 			// terminal: true,
 			scope: {
-				group: '='
+				group: '=',
+                firstFields: '='
 			}, // {} = isolate, true = child, false/undefined = no change
-			// controller: function($scope, $element, $attrs, $transclude) {},
+			/*controller: function($scope, $element, $attrs, $transclude) {
+                console.log("The passed in firstField is --> ", $scope.firstField);
+            },*/
 			// require: 'ngModel', // Array = multiple requires, ? = optional, ^ = check parent elements
-			restrict: 'E', // E = Element, A = Attribute, C = Class, M = Comment
+			restrict: 'AEC', // E = Element, A = Attribute, C = Class, M = Comment
 			// template: '',
 			templateUrl: './partials/query-builder.html',
 			// replace: true,
 			// transclude: true,
 			compile: function(tElement, tAttrs) {
+                var tq = String.fromCharCode(9775)+" -- ";
+
                 var content, directive;
                 content = tElement.contents().remove();
-				// linking
-				return function (scope, elm, attrs){
-					scope.operators = [
+                // This content is every element in query-builder.html, in short, it is the query-builder.html
+                // console.log("beginning (content) --> "+tq, content);
+                // This tElement is the query builder itself...
+                // console.log("beginning (tElement) --> "+tq, tElement);
+                // at beginning directive is undefined. this directive is actually current directive (queryBuilder).
+                // console.log("beginning (directive) --> "+tq+" this directive is actually current directive (queryBuilder). But it is only triggered when the Add Group btn clikced. thus at the beginning, it is undefined. Result -- ", directive);
+                
+
+                // linking
+                return function linking(scope, elm, attrs){
+
+                    scope.operators = [
                     { name: 'AND' },
                     { name: 'OR' }];
 
-                    scope.fields = [
+                    // first field... Actually, the data of it should be received from backend...
+                    /*scope.fields = [
                     { name: 'Firstname' },
                     { name: 'Lastname' },
                     { name: 'Birthdate' },
                     { name: 'City' },
-                    { name: 'Country' }];
+                    { name: 'Country' }];*/
 
                     scope.conditions = [
                     { name: '=' },
@@ -38,19 +53,44 @@
                     { name: '>' },
                     { name: '>=' }];
 
+                    console.log("¿¿¿¿¿¿¿¿¿", scope.firstFields);
                     scope.addCondition = function () {
+                        // add new condition to rules
+                        // the field here will match the content in arr firstFields (Lastname === Lastname),
+                        // if the name matched, it would be the first option displayed.
+                        // don't konw how. will find out...
                         scope.group.rules.push({
                             condition: '=',
                             field: 'Firstname',
-                            data: ''
+                            data: '',
+                            tmplUrl: ''
+                        });
+                        // console.log(tq+'the condiiton is :--> ', scope.group.rules);
+                        console.log('one example --> '+tq+" -Due to the group has been built, the computed would be executed to form the rules data to group Obj...");
+                        console.log('the pass in group is --> ', scope.group);
+
+                        // pickupTemplateForLastField (scope.group.rules[0].field);
+                        pickupTemplateForLastField (scope.group.rules);
+                    };
+
+                    function pickupTemplateForLastField (theRulesArr) {
+                        console.log(tq+'˘¿˘˘˘˘˘¿¿¿ theRulesArr is --> ', theRulesArr);
+                        theRulesArr.forEach(function (elem, index) {
+                            if (elem.field === 'Firstname') {
+                                elem.templateUrl = './partials/template2.html';
+                            } else{
+                                elem.templateUrl = './partials/template1.html';
+                            };
                         });
                     };
 
                     scope.removeCondition = function (index) {
+                        // remove the pointed rule
                         scope.group.rules.splice(index, 1);
                     };
 
                     scope.addGroup = function () {
+                        // add one more group
                         scope.group.rules.push({
                             group: {
                                 operator: 'AND',
@@ -60,18 +100,32 @@
                     };
 
                     scope.removeGroup = function () {
+                        // remove current group
                         "group" in scope.$parent && scope.$parent.group.rules.splice(scope.$parent.$index, 1);
                     };
 
                     directive || (directive = $compile(content));
 
                     elm.append(directive(scope, function ($compile) {
+                        // the directive is about compile new added group into current directive...
+                        console.log(tq+" directive is: ", directive);
                         return $compile;
                     }));
 
 				}
+
+                /*var ff = $('#firstField');
+                console.log('the first field valuel is --> '+ff.val());
+                console.log('the first field text is --> '+ff.text());
+                console.log('the first field html is --> '+ff.html());*/
+
 			}
-			// link: function($scope, iElm, iAttrs, controller) {}
+			/*link: function($scope, iElm, iAttrs, ctrl) {
+                var ff = iElm.find('#firstField');
+                console.log('the first field valuel is --> '+ff.val());
+                console.log('the first field text is --> '+ff.text());
+                console.log('the first field html is --> '+ff.html());
+            }*/
 		};
 	}]);
 
